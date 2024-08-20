2ef148872d37e770911e73fd159fb838e923c9d8
Amirali <a.sahraei98@gmail.com>
2024-08-20 18:54:20 +0400

    add test.py

--- app/test.py
+++ app/test.py
@@ -1,6 +1,7 @@
 import unittest
 from flask_testing import TestCase
-from server import app, gpt_server
+from server import app
+import io
 
 
 class FlaskTestCase(TestCase):
@@ -12,28 +13,92 @@ class FlaskTestCase(TestCase):
     def test_index(self):
         """Test that the index page loads correctly."""
         response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Welcome to ChatGPT", response.data)
+        self.assertEqual(response.status_code, 302)
+        self.assertIn("/login", response.location)
 
-    def test_chat_get(self):
+    def test_login_successful(self):
+        response = self.client.post(
+            "/login", data={"email": "a.sahraei98@gmail.com", "password": "amir123"}
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual("/chat", response.location)
+
+    def test_login_error(self):
+        response = self.client.post(
+            "/login", data={"email": "a@gmail.com", "password": "amir123"}
+        )
+        self.assertEqual(response.status_code, 404)
+        self.assertIn(b"Error Page", response.data)
+
+    def test_signup_successful(self):
+        response = self.client.post(
+            "/signup",
+            data={
+                "firstname": "Karen5",
+                "lastname": "Vanry5",
+                "email": "karen5@gmail.com",
+                "password": "123",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual("/login", response.location)
+
+    def test_signup_error(self):
+        response = self.client.post(
+            "/signup",
+            data={
+                "firstname": "amir",
+                "lastname": "sh",
+                "email": "a.sahraei98@gmail.com",
+                "password": "123",
+            },
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertIn(b"Error Page", response.data)
+
+    def test_file_upload(self):
+        with self.client.session_transaction() as sess:
+            sess["user_email"] = "a.sahraei98@gmail.com"
+        with open("Amirali_Sahraei_CV_OLD.pdf", "rb") as f:
+            data = {}
+            data["file"] = f
+            response = self.client.post(
+                "/fileUpload", data=data, content_type="multipart/form-data"
+            )
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual("/chat", response.location)
+
+    def test_chat_get_logged_in(self):
         """Test GET request to /chat."""
+        with self.client.session_transaction() as sess:
+            sess["user_email"] = "a.sahraei98@gmail.com"
         response = self.client.get("/chat")
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Welcome to ChatGPT", response.data)
+        self.assertIn(b"Welcome", response.data)
 
-    def test_chat_post(self):
+    def test_chat_get_not_logged_in(self):
+        """Test GET request to /chat."""
+        with self.client.session_transaction() as sess:
+            sess.pop("user_email", None)
+        response = self.client.get("/chat")
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual("/login", response.location)
+
+    def test_chat_post_logged_in(self):
         """Test POST request to /chat."""
+        with self.client.session_transaction() as sess:
+            sess["user_email"] = "a.sahraei98@gmail.com"
         response = self.client.post("/chat", data={"text": "Hello"})
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Hello", response.data)
-        self.assertIn(b"Hello", response.data)
 
-    def test_gpt_server(self):
-        """Test the GPT server response."""
-        chat_history = [("Hello", "Hi there!")]
-        response = gpt_server("How are you?", chat_history)
-        self.assertIsInstance(response, str)
-        self.assertNotEqual(response, "")
+    def test_chat_post_not_logged_in(self):
+        """Test POST request to /chat."""
+        with self.client.session_transaction() as sess:
+            sess.pop("user_email", None)
+        response = self.client.post("/chat", data={"text": "Hello"})
+        self.assertEqual(response.status_code, 302)
+        self.assertIn("/login", response.location)
 
 
 if __name__ == "__main__":
